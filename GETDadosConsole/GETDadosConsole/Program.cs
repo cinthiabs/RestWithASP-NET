ad0e3811fb969284b594c9a4df1d27e26c2d9dd0
cinthiabs <cinthiabarbosa8d@outlook.com>
2022-01-23 20:23:17 -0300

    Console

---
+++ GETDadosConsole/GETDadosConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GETDadosConsole
+{
+    class Program
+    {
+        static  async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient { BaseAddress = new Uri("https://jsonplaceholder.typicode.com") };
+            var response = await client.GetAsync("users");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<Users[]>(content);
+
+            foreach (var item in users)
+            {
+                Console.WriteLine(item.Username +" - "+ item.Email);
+            }
+        }
+    }
+}
