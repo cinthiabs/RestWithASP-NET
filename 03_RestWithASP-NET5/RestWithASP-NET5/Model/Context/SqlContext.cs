0d68db0da538f532c488f0db7ac2ac21acd0cd81
cinthiabs <cinthiabarbosa8d@outlook.com>
2022-01-27 08:22:55 -0300

    API

---
+++ 03_RestWithASP-NET5/RestWithASP-NET5/Model/Context/SqlContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Model.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
