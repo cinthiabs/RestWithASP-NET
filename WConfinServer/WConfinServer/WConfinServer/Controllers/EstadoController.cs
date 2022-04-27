b9219f82217fdba8f76fc5b9eafd628c196b6bd8
cinthiabs <cinthiabarbosa8d@outlook.com>
2022-04-27 19:48:26 -0300

    API

---
+++ WConfinServer/WConfinServer/WConfinServer/Controllers/EstadoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WConfinServer.Models;
+
+namespace WConfinServer.Controllers
+{
+    [Route("api/[controller]")] // chamar a rota https://localhost:44300/api/Estado
+    [ApiController]
+    public class EstadoController : ControllerBase
+    {
+        public static List<Estado> lista = new List<Estado>();
+
+        [HttpGet]
+        public List<Estado> GetEstados()
+        {
+            return lista;
+        }
+
+        [HttpPost]
+        public string PostEstado(Estado estado)
+        {
+            lista.Add(estado);
+
+            return "Estado cadastrado com sucesso!";
+        }
+
+        [HttpPut]
+        public string PutEstado(Estado estado)
+        {
+            Estado estadoAux = lista.Where(x => x.Sigla == estado.Sigla).FirstOrDefault();
+            estadoAux.Nome = estado.Nome;
+            return "Estado alterado com sucesso!";
+        }
+
+        [HttpDelete]
+        public string DeleteEstado(Estado estado)
+        {
+            Estado estadoAux = lista.Where(x => x.Sigla == estado.Sigla).FirstOrDefault();
+            lista.Remove(estadoAux);
+            return "Estado excluido com sucesso!";
+        }
+    }
+}
---
+++ WConfinServer/WConfinServer/WConfinServer/Models/Estado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WConfinServer.Models
+{
+    public class Estado
+    {
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+    }
+}
